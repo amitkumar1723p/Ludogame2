b161a4577d1b72dae9c9e987cc515aade2d79e04
Amit <developeramit001@gmail.com>
2025-06-06 21:07:50 +0530

    Ludogame

--- src/components/Dice.jsx
+++ src/components/Dice.jsx
@@ -76,8 +76,8 @@ const Dice = React.memo(({ color, data, player }) => {
 
   const handleDicePress = async () => {
     //  playSound('dice_roll');
-    const newDiceNo = Math.floor(Math.random() * 6) + 1;
-    //  const  newDiceNo =5
+    // const newDiceNo = Math.floor(Math.random() * 6) + 1;
+     const  newDiceNo =6
 
     playSound("dice_roll")
     setDiceRolling(true);
